372b4ed9a4997822aeb70961cdb29ce726425c5a
mendess <pedro.mendes.26@gmail.com>
2020-08-25 09:45:48 +0100

    Backup m | tolaria@25/08/20 09:45

--- mpvsocket.sh
+++ mpvsocket.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
-if [ -e /tmp ]; then
-    TMPDIR=/tmp
-else
-    TMPDIR="$HOME"
+if [ -z "$TMPDIR" ]; then
+    if [ -e /tmp ]; then
+        TMPDIR=/tmp
+    else
+        TMPDIR="$HOME/.cache"
+    fi
 fi
 
-#shellcheck disable=SC2009
-last="$(ps -ef |
-    grep -v grep |
-    grep -oP '\.mpvsocket[0-9]+' |
-    sed -E 's/\.mpvsocket([0-9]+)/\1/' |
-    sort -V |
-    uniq |
-    tail -1)"
-if [ -z "$last" ]; then
-    if [ $# -gt 0 ] && [ "$1" = new ]; then
-        echo "$TMPDIR/.mpvsocket0"
-    else
-        echo "/dev/null"
-    fi
-else
-    if [ $# -gt 0 ] && [ "$1" = new ]; then
-        echo "$TMPDIR/.mpvsocket$((++last))"
-    else
-        echo "$TMPDIR/.mpvsocket$last"
-    fi
-fi
+CACHE_SOCKET="$HOME/.cache/mpvsocket_cache"
+
+last() {
+    case "$1" in
+        num) r='[0-9]+' ;;
+        *) r='[0-9]+|_cache' ;;
+    esac
+    #shellcheck disable=SC2009
+    ps -ef |
+        grep -v grep |
+        grep -oP 'mpvsocket('"$r"')' |
+        sed -E 's/mpvsocket('"$r"')/\1/' |
+        sort -V |
+        uniq |
+        tail -1
+}
+
+case "$1" in
+    new)
+        last="$(last num)"
+        if [ "$last" ]; then
+            echo "$TMPDIR/.mpvsocket$((++last))"
+        else
+            echo "$TMPDIR/.mpvsocket0"
+        fi
+        ;;
+    cache) echo "$CACHE_SOCKET" ;;
+    '')
+        last="$(last)"
+        case "$last" in
+            _cache) echo "$CACHE_SOCKET" ;;
+            '') echo /dev/null ;;
+            *) echo "$TMPDIR/.mpvsocket$last" ;;
+        esac
+        ;;
+esac
