59b025c6653ffa6d8704caa69a2995e194982360
mendess <pedro.mendes.26@gmail.com>
2020-09-20 22:19:05 +0100

    Remove android app

--- jukebox/app/app/src/main/java/xyz/mendess/jukebox/JukeboxLib.java
+++
@@ -1,9 +0,0 @@
-package xyz.mendess.jukebox;
-
-public class JukeboxLib {
-    public static final String LOG_TAG = "jukebox.so";
-
-    public static native void startUserThread(String addr);
-    public static native void setRoomName(String s);
-    public static native void sendCommand(String s);
-}
--- jukebox/app/app/src/main/java/xyz/mendess/jukebox/ui/main/HomeViewModel.kt
+++
@@ -1,7 +0,0 @@
-package xyz.mendess.jukebox.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
