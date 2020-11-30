071f5f606fd84f2ffeac56d734a0a6c2bca86497
mendess <pedro.mendes.26@gmail.com>
2020-11-30 21:07:51 +0000

    Fix current --link

--- m.sh
+++ m.sh
@@ -383,7 +383,7 @@ current_song() {
     local filename videoId chapter categories up_next
     videoId="$(mpv_get filename --raw-output '.data' |
         sed -E 's/.*-([a-zA-Z0-9\-_-]{11})(=m)?.*/\1/g')"
-    [[ "$1" =~ (-i|--link) ]] && notify "https://youtu.be/$videoId" && return
+    [[ "$1" =~ (-i|--link) ]] && notify "https://youtu.be/${videoId/.m4a/}" && return
 
     filename=$(mpv_get media-title --raw-output '.data')
 
